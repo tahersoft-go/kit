7643ab94e8ab36c741efeb25b1e1a8e8e03a1e52
Taher <mhosseintaher@gmail.com>
2024-02-18 10:48:37 +0330

    added some date and boolean support to filters

--- filter/dboperators.go
+++ filter/dboperators.go
@@ -25,22 +25,28 @@ const (
 	CONTAINS                   = "contains"
 	STARTS_WITH                = "startsWith"
 	ENDS_WITH                  = "endsWith"
-	IS_EMPTY                   = "isEmpty"
-	IS_NOT_EMPTY               = "isNotEmpty"
-	IS_ANY_OF                  = "isAnyOf"
+	IS                         = "is"         // boolean is and date is
+	IS_EMPTY                   = "isEmpty"    // works for string, date, number
+	IS_NOT_EMPTY               = "isNotEmpty" // works for string, date, number
+	IS_ANY_OF                  = "isAnyOf"    // works for string, date, number
 	NUMBER_EQUALS              = "="
 	NUMBER_NOT_EQUALS          = "!="
 	NUMBER_GREATER_THAN        = ">"
 	NUMBER_GREATER_THAN_EQUALS = ">="
 	NUMBER_LESS_THAN           = "<"
 	NUMBER_LESS_THAN_EQUALS    = "<="
+	DATE_IS_NOT                = "not"
+	DATE_IS_AFTER              = "after"
+	DATE_IS_ON_OR_AFTER        = "onOrAfter"
+	DATE_IS_BEFORE             = "before"
+	DATE_IS_ON_OR_BEFORE       = "onOrBefore"
 )
 
-var SQL = map[string]string{
+var MapSqlOperators = map[string]string{
 	EQUALS:                     "=",
-	CONTAINS:                   "LIKE",
-	STARTS_WITH:                "LIKE",
-	ENDS_WITH:                  "LIKE",
+	CONTAINS:                   "ILIKE",
+	STARTS_WITH:                "ILIKE",
+	ENDS_WITH:                  "ILIKE",
 	IS_EMPTY:                   "IS NULL",
 	IS_NOT_EMPTY:               "IS NOT NULL",
 	IS_ANY_OF:                  "IN",
@@ -50,6 +56,12 @@ var SQL = map[string]string{
 	NUMBER_GREATER_THAN_EQUALS: ">=",
 	NUMBER_LESS_THAN:           "<",
 	NUMBER_LESS_THAN_EQUALS:    "<=",
+	IS:                         "=",
+	DATE_IS_NOT:                "!=",
+	DATE_IS_AFTER:              ">",
+	DATE_IS_BEFORE:             "<",
+	DATE_IS_ON_OR_AFTER:        ">=",
+	DATE_IS_ON_OR_BEFORE:       "<=",
 }
 
 type DBOperatorAndValue struct {
@@ -59,46 +71,36 @@ type DBOperatorAndValue struct {
 
 // GetDBOperatorAndValue returns the database operator and value for a given operator and value
 func GetDBOperatorAndValue(op, value string) DBOperatorAndValue {
-	caseSensitive := false
-
-	likeOperator := "LIKE"
-	equalsOperator := "="
-
-	if !caseSensitive {
-		// Use ILIKE for PostgreSQL or use LOWER for other databases
-		likeOperator = "ILIKE"   // or use LOWER(column) LIKE LOWER(value) in the calling function
-		equalsOperator = "ILIKE" // or use LOWER(column) = LOWER(value) in the calling function
-	}
 
 	switch op {
 	case CONTAINS:
 		return DBOperatorAndValue{
-			Operator: likeOperator,
+			Operator: MapSqlOperators[CONTAINS],
 			Value:    "%" + value + "%",
 		}
 	case STARTS_WITH:
 		return DBOperatorAndValue{
-			Operator: likeOperator,
+			Operator: MapSqlOperators[STARTS_WITH],
 			Value:    value + "%",
 		}
 	case ENDS_WITH:
 		return DBOperatorAndValue{
-			Operator: likeOperator,
+			Operator: MapSqlOperators[ENDS_WITH],
 			Value:    "%" + value,
 		}
 	case EQUALS:
 		return DBOperatorAndValue{
-			Operator: equalsOperator,
+			Operator: MapSqlOperators[EQUALS],
 			Value:    value,
 		}
 	case IS_EMPTY:
 		return DBOperatorAndValue{
-			Operator: "IS NULL",
+			Operator: MapSqlOperators[IS_EMPTY],
 			Value:    "",
 		}
 	case IS_NOT_EMPTY:
 		return DBOperatorAndValue{
-			Operator: "IS NOT NULL",
+			Operator: MapSqlOperators[IS_NOT_EMPTY],
 			Value:    "",
 		}
 	case IS_ANY_OF:
@@ -107,43 +109,72 @@ func GetDBOperatorAndValue(op, value string) DBOperatorAndValue {
 			strSlice[i] = "'" + v + "'"
 		}
 		return DBOperatorAndValue{
-			Operator: "IN",
+			Operator: MapSqlOperators[IS_ANY_OF],
 			Value:    fmt.Sprintf("(%s)", strings.Join(strSlice, ",")),
 		}
 	case NUMBER_EQUALS:
 		return DBOperatorAndValue{
-			Operator: "=",
+			Operator: MapSqlOperators[NUMBER_EQUALS],
 			Value:    value,
 		}
 	case NUMBER_NOT_EQUALS:
 		return DBOperatorAndValue{
-			Operator: "!=",
+			Operator: MapSqlOperators[NUMBER_NOT_EQUALS],
 			Value:    value,
 		}
 	case NUMBER_GREATER_THAN:
 		return DBOperatorAndValue{
-			Operator: ">",
+			Operator: MapSqlOperators[NUMBER_GREATER_THAN],
 			Value:    value,
 		}
 	case NUMBER_GREATER_THAN_EQUALS:
 		return DBOperatorAndValue{
-			Operator: ">=",
+			Operator: MapSqlOperators[NUMBER_GREATER_THAN_EQUALS],
 			Value:    value,
 		}
 	case NUMBER_LESS_THAN:
 		return DBOperatorAndValue{
-			Operator: "<",
+			Operator: MapSqlOperators[NUMBER_LESS_THAN],
 			Value:    value,
 		}
 	case NUMBER_LESS_THAN_EQUALS:
 		return DBOperatorAndValue{
-			Operator: "<=",
+			Operator: MapSqlOperators[NUMBER_LESS_THAN_EQUALS],
 			Value:    value,
 		}
-
+	case IS:
+		return DBOperatorAndValue{
+			Operator: MapSqlOperators[IS],
+			Value:    value,
+		}
+	case DATE_IS_NOT:
+		return DBOperatorAndValue{
+			Operator: MapSqlOperators[DATE_IS_NOT],
+			Value:    value,
+		}
+	case DATE_IS_AFTER:
+		return DBOperatorAndValue{
+			Operator: MapSqlOperators[DATE_IS_AFTER],
+			Value:    value,
+		}
+	case DATE_IS_BEFORE:
+		return DBOperatorAndValue{
+			Operator: MapSqlOperators[DATE_IS_BEFORE],
+			Value:    value,
+		}
+	case DATE_IS_ON_OR_AFTER:
+		return DBOperatorAndValue{
+			Operator: MapSqlOperators[DATE_IS_ON_OR_AFTER],
+			Value:    value,
+		}
+	case DATE_IS_ON_OR_BEFORE:
+		return DBOperatorAndValue{
+			Operator: MapSqlOperators[DATE_IS_ON_OR_BEFORE],
+			Value:    value,
+		}
 	}
 	return DBOperatorAndValue{
-		Operator: "=",
+		Operator: MapSqlOperators[EQUALS],
 		Value:    value,
 	}
 }
